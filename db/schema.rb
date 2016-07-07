21c7158081e0ed64025f4df18a617c037ea2f396
smith <user@localhost.localdomain>
2016-07-07 09:40:46 -0400

    lesson 13

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614113416) do
+ActiveRecord::Schema.define(version: 20160707124320) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160614113416) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "current_station_id"
+    t.integer  "route_id"
   end
 
   create_table "users", force: :cascade do |t|
